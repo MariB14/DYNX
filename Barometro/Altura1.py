92b0587f89a0df9c5e7cb92b2db473d3edc3b283
MariB14 <marybandasosa@gmail.com>
2026-07-08 13:32:53 -0600

    calibracion

--- Barometro/Altura1.py
+++ Barometro/Altura1.py
@@ -2,18 +2,20 @@ try:
     from smbus2 import SMBus
 except ImportError:
     from smbus import SMBus
-from bme280 import BME280
-from time import sleep
+import bme280
 
 # Iniciando el BME280
 bus = SMBus(1)
-bme280 = BME280(i2c_dev=bus)
+calibration_params = bme280.load_calibration_params(bus, 0x76)
 
-PRESION_NIVEL_MAR = 1013.25  # presión estándar a nivel del mar en hPa
+from time import sleep
+
+PRESION_NIVEL_MAR = 1013.25
 
 while True:
-    presion = bme280.get_pressure()
-    altura = 44330.0 * (1.0 - (presion / PRESION_NIVEL_MAR) ** 0.1903)
+    data = bme280.sample(bus, 0x76, calibration_params)
+    presion     = data.pressure
+    altura      = 44330.0 * (1.0 - (presion / PRESION_NIVEL_MAR) ** 0.1903)
 
     print(f"Presion:     {presion:.2f} hPa")
     print(f"Altura:      {altura:.2f} m")
