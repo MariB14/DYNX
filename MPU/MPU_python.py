a38f257613ede6882d57a29a54bcb5f7a56f71dd
Kristalryna <kristaloram04@gmail.com>
2026-07-08 12:03:14 -0600

    correccion while true

--- MPU/MPU_python.py
+++ MPU/MPU_python.py
@@ -65,34 +65,35 @@ MPU_Init()
 print (" Reading Data of Gyroscope and Accelerometer")
 
 while True:
-	
-	#Read Accelerometer raw value
-	acc_x = read_raw_data(ACCEL_XOUT_H)
-	acc_y = read_raw_data(ACCEL_YOUT_H)
-	acc_z = read_raw_data(ACCEL_ZOUT_H)
-	
-	#Read Gyroscope raw value
-	gyro_x = read_raw_data(GYRO_XOUT_H)
-	gyro_y = read_raw_data(GYRO_YOUT_H)
-	gyro_z = read_raw_data(GYRO_ZOUT_H)
-	
-	#Full scale range +/- 250 degree/C as per sensitivity scale factor
-	Ax = acc_x/16384.0
-	Ay = acc_y/16384.0
-	Az = acc_z/16384.0
-	
-	Gx = gyro_x/131.0
-	Gy = gyro_y/131.0
-	Gz = gyro_z/131.0
- 
-while True:
-    print("\tX=%.2f g" % Ax, "\tY=%.2f g" % Ay, "\tZ=%.2f g" % Az)
+
+    # Leer acelerómetro
+    acc_x = read_raw_data(ACCEL_XOUT_H)
+    acc_y = read_raw_data(ACCEL_YOUT_H)
+    acc_z = read_raw_data(ACCEL_ZOUT_H)
+
+    # Leer giroscopio
+    gyro_x = read_raw_data(GYRO_XOUT_H)
+    gyro_y = read_raw_data(GYRO_YOUT_H)
+    gyro_z = read_raw_data(GYRO_ZOUT_H)
+
+    # Conversión
+    Ax = acc_x / 16384.0
+    Ay = acc_y / 16384.0
+    Az = acc_z / 16384.0
+
+    Gx = gyro_x / 131.0
+    Gy = gyro_y / 131.0
+    Gz = gyro_z / 131.0
+
+    print("\tX=%.2f g" % Ax,
+          "\tY=%.2f g" % Ay,
+          "\tZ=%.2f g" % Az)
 
     if abs(Ax) >= UMBRAL or abs(Ay) >= UMBRAL or abs(Az) >= UMBRAL:
         GPIO.output(ledPin, GPIO.HIGH)
-        print("LED ACTIVADO - Se detectaron 90 grados")
+        print("LED ACTIVADO")
     else:
         GPIO.output(ledPin, GPIO.LOW)
-        print("LED APAGADO - Sin inclinación de 90 grados")
+        print("LED APAGADO")
 
     sleep(1)
