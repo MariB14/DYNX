13fe0f2aa6b78c47a22df639c4dbc90d630482a1
MariB14 <marybandasosa@gmail.com>
2026-07-15 12:20:13 -0600

    Re acomodo de los codigos y creacion del sh
    Moví los codigos del mpu y barometro a una carpeta en comun con u sh para para ejecutarlos al mismo tiempo

---
+++ Sensores_comun/Simultaneos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Iniciando sistemas de telemetría..."
+
+# 1. Iniciar los scripts de Python en segundo plano (usando &)
+python3 MPU6050.py &
+python3 Altura1.py &
+
+# Cuando tengas los demás listos, solo quítales el #
+# python3 GPS.py &
+# python3 Transmisor_RF.py &
+
+echo "Todos los sistemas corriendo simultáneamente."
+echo "Presiona Ctrl+C para detener todos los sensores."
+
+# 2. Esta línea mágica atrapa el Ctrl+C y apaga los códigos en segundo plano
+trap "echo -e '\n🛑 Apagando sensores...'; pkill -P $$; exit" SIGINT
+
+# 3. Mantener el script principal vivo esperando
+wait
