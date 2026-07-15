51c38af6776886c5eec8df30470a403d480bca9a
MariB14 <marybandasosa@gmail.com>
2026-07-15 14:51:17 -0600

    intento de enviar datos por serial

--- Sensores_comun/Simultaneos.sh
+++ Sensores_comun/Simultaneos.sh
@@ -4,6 +4,7 @@ echo "Iniciando sistemas de telemetría..."
 # 1. Iniciar los scripts de Python en segundo plano (usando &)
 python3 MPU_python.py &
 python3 Altura1.py &
+python3 enviardata.py &
 
 # Cuando tengas los demás listos, solo quítales el #
 # python3 GPS.py &
@@ -13,7 +14,7 @@ echo "Todos los sistemas corriendo simultáneamente."
 echo "Presiona Ctrl+C para detener todos los sensores."
 
 # 2. Esta línea mágica atrapa el Ctrl+C y apaga los códigos en segundo plano
-trap "echo -e '\n🛑 Apagando sensores...'; pkill -P $$; exit" SIGINT
+trap "echo -e '\n Apagando sensores...'; pkill -P $$; exit" SIGINT
 
 # 3. Mantener el script principal vivo esperando
 wait
